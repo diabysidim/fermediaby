f3ff41da9b02dd0b0b9c49a7f13ddd06c121c9a1
Sidi <diaby002@umn.edu>
2020-08-03 14:49:27 -0500

    first note

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,22 @@
-exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-      node: {
-        fs: 'empty'
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//     actions.setWebpackConfig({
+//       node: {
+//         fs: 'empty'
+//       }
+//     })
+//   }
+
+  exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    if (stage === 'build-javascript') {
+      const config = getConfig()
+      const miniCssExtractPlugin = config.plugins.find(
+        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+      )
+      if (miniCssExtractPlugin) {
+        miniCssExtractPlugin.options.ignoreOrder = true
       }
-    })
+      actions.replaceWebpackConfig(config)
+    }
   }
 
 
